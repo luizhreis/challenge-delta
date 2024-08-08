91e44cdc789245bf39db47f2786a7118a115d3fa
Luiz Reis <luizhreis.gris@gmail.com>
2024-08-07 23:27:27 -0300

    feat: Add hpa and metrics server to script

--- run-app.sh
+++ run-app.sh
@@ -2,6 +2,7 @@
 minikube start
 
 eval $(minikube docker-env)
+kubectl apply -f k8s/metrics-server.yml
 
 docker build -t backend-container:20240805 ./app/backend/
 
@@ -19,8 +20,10 @@ kubectl apply -f k8s/backend-deployment.yml
 kubectl apply -f k8s/backend-service.yml
 kubectl apply -f k8s/frontend-deployment.yml
 kubectl apply -f k8s/frontend-service.yml
+kubectl apply -f k8s/frontend-hpa.yml
 
 sleep 120
 
+echo "\nAPP URL:"
 minikube service frontend --url
 
